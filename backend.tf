78587763f88d11308d9e1e6fb4d07110bb0a9831
Subhranil Ghosh <subhranil.ghosh@tmdc.io>
2022-11-16 15:20:18 +0530

    updated

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "circleci-terraform"
-    key = "circleci/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
--- ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "myec2" {
-  ami           = data.aws_ami.myami.id
-  instance_type = "t3.micro"
-  key_name = var.key_pair
-  tags = {
-    Name = "my-Ec2"
-  }
-}
